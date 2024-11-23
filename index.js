b93da160801cc68fbfac9abc4428cbc67a0897b8
Filip Buljan <filipbuljan2002@gmail.com>
2024-11-23 11:53:21 +0100

    remove unnecessary db access

--- index.js
+++ index.js
@@ -15,34 +15,13 @@ const db = new sqlite3.Database('./database.db', (err) => {
   }
 });
 
-db.run(
-  `CREATE TABLE IF NOT EXISTS test (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
-  )`,
-  (err) => {
+app.get("/", (req, res) => {
+  db.all(`SELECT * FROM test`, [], (err, rows) => {
     if (err) {
-      console.error('Error creating table:', err.message);
+      console.error("Error retrieving data:", err.message);
+      res.status(500).send("Failed to retrieve data.");
     } else {
-      console.log('Table created or already exists.');
-    }
-  }
-);
-
-app.get('/', (req, res) => {
-  db.run(`INSERT INTO test (name) VALUES (?)`, ['Zicer 2024'], (err) => {
-    if (err) {
-      console.error('Error inserting data:', err.message);
-      res.status(500).send('Failed to insert data.');
-    } else {
-      db.all(`SELECT * FROM test`, [], (err, rows) => {
-        if (err) {
-          console.error('Error retrieving data:', err.message);
-          res.status(500).send('Failed to retrieve data.');
-        } else {
-          res.json(rows);
-        }
-      });
+      res.json(rows);
     }
   });
 });
